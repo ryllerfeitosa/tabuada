9b0816809bd5b7c68eeaabf8a3e78297bf93e0a2
RILLER DA COSTA FEITOSA <25203652@CFP127ALUNO.LOCAL>
2025-11-03 11:28:19 -0300

    Stage, cena e objetos criados

--- src/main/java/br/senai/sp/jandira/tabuada/HelloApplication.java
+++ src/main/java/br/senai/sp/jandira/tabuada/HelloApplication.java
@@ -3,6 +3,9 @@ package br.senai.sp.jandira.tabuada;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,11 +22,53 @@ public class HelloApplication extends Application {
         stage.setWidth(500);
         stage.setHeight(500);
 
-        //
+        //cabeçalho da tela
         VBox header = new VBox();
         header.setStyle("-fx-padding: 10;-fx-background-color: blue");
 
-        scene.setRoot(header);
+        //Adicionar um label ao header
+        Label labelTitulo = new Label("Tabuada");
+        labelTitulo.setStyle("-fx-text-fill: white;-fx-font-size: 30;-fx-font-weight: bold");
+        Label labelSubTitulo= new Label("Construa tabuadas sem limites");
+        labelSubTitulo.setStyle("-fx-text-fill: beige;-fx-font-size: 15");
+
+        header.getChildren().add(labelTitulo);
+        header.getChildren().add(labelSubTitulo);
+
+        //Colocar o header no root
+        root.getChildren().addAll(header);
+
+        //Criar o multiplicando
+        HBox multiplicando = new HBox();
+        Label labelMultiplicando = new Label("Multiplicando");
+        TextField textFieldMultiplicando = new TextField();
+
+        multiplicando.getChildren().add(labelMultiplicando);
+        multiplicando.getChildren().add(textFieldMultiplicando);
+
+        //colocando o multiplicando no root
+        root.getChildren().add(multiplicando);
+
+        HBox minMultiplicador = new HBox();
+        Label labelMinMultiplicador = new Label("MinMultiplicador");
+        TextField textFieldMinMultiplicador = new TextField();
+
+        minMultiplicador.getChildren().add(labelMinMultiplicador);
+        minMultiplicador.getChildren().add(textFieldMinMultiplicador);
+
+        //colocando o minMultiplicador no root
+        root.getChildren().add(minMultiplicador);
+
+        HBox maxMultiplicador = new HBox();
+        Label labelMaxMultiplicador = new Label("MaxMultiplicador");
+        TextField textFieldMaxMultiplicador = new TextField();
+
+        maxMultiplicador.getChildren().add(labelMaxMultiplicador);
+        maxMultiplicador.getChildren().add(textFieldMaxMultiplicador);
+
+        //colocando o maxMultiplicador no root
+        root.getChildren().add(maxMultiplicador);
+
         stage.setScene(scene);
         stage.setTitle("Tabuada");
         stage.show();
